c718a6491af5791c721b3ebae94a241a81348b29
Alex <alexandra.bulgak@gmail.com>
2024-02-28 22:45:31 +0100

    HW

--- Lesson3/test_negative.py
+++ Lesson3/test_negative.py
@@ -8,8 +8,8 @@ with open('config.yaml') as f:
 
 class TestNegative:
     def test_negative_step1(self, make_files, make_folders, make_bad_file):
-        assert negative_checkout(f"cd {data['FOLDER_OUT']}; 7z e {make_bad_file}.7z {data['FOLDER_EXTRACT']} -y", "ERROR"), "test1 FAIL"
+        assert negative_checkout(f"cd {data['FOLDER_OUT']}; 7z e {make_bad_file}.{data['extension']} {data['FOLDER_EXTRACT']} -y", "ERROR"), "test1 FAIL"
 
 
     def test_negative_step2(self, make_files, make_folders, make_bad_file):
-        assert negative_checkout(f"cd {data['FOLDER_OUT']}; 7z t {make_bad_file}.7z -o{data['FOLDER_EXTRACT']} -y", "ERROR"), "test2 FAIL"
+        assert negative_checkout(f"cd {data['FOLDER_OUT']}; 7z t {make_bad_file}.{data['extension']} -o{data['FOLDER_EXTRACT']} -y", "ERROR"), "test2 FAIL"
